f45df7092fc5e1a88db570bd05067aad48416557
Gator1218 <ericsmith@ge.com>
2015-10-23 16:35:53 -0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,29 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Functions makeCacheMatrix and cachSolve calculate the inverse of a matrix 
+## and store it in cache so that it does not need to be re-solved later.
 
-## Write a short comment describing this function
+## create variables for original matrix and inverse matrix
+OrigMatrix <- NULL
+InvMatrix <- NULL
+Arb <- matrix(c(1, 5, 2.3, 2.1),2,2) ##arbitrary matrix to uses as default
 
-makeCacheMatrix <- function(x = matrix()) {
-
+##this function calculates and caches the inverse of the passed matrix
+makeCacheMatrix <- function(x) {
+      ##create inverse matrix
+            InvMatrix <- solve(x)
+      ##cache orig and inv in global environment
+            OrigMatrix <<- x
+            InvMatrix <<- InvMatrix
+      ##return inverse matrix
+            InvMatrix
 }
-
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## function below checks if the inverse matrix is already stored in memory and
+## calls makeCacheMatrix if it is not already cached
+cacheSolve <- function(x = Arb) {
+      ## return inverse matrix from memory if it is already cached
+      if(x==OrigMatrix && (!is.null(InvMatrix))) {
+            message ("getting cached data")
+            return (InvMatrix)
+      }
+      ## otherwise call makeCacheMatrix to calculate the inverse
+      makeCacheMatrix(x)
 }
